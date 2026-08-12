edd669be4f08369f70080433fe92f27fe6cd164d
Heyder Medina <medinaheyder@gmail.com>
2026-08-12 15:05:15 -0600

    feat: create SquareCell component for rendering individual game board cells

---
+++ src/components/SquareCell.tsx
@@ -0,0 +1,28 @@
+interface SquareCellProps {
+    index: number;
+    value: string | null;
+    updateBoard: (index: number) => void;
+}
+
+export function SquareCell({
+    index,
+    value,
+    updateBoard,
+}: SquareCellProps) {
+
+    const handleClick = () => {
+        updateBoard(index);
+    }
+
+    return (
+        <button
+            onClick={handleClick}
+            className={`grid place-items-center aspect-square w-24 
+            border border-black text-2xl transition-colors
+            ${value === 'x' ? "bg-red-600 text-white" :
+              value === 'o' ? "bg-blue-600 text-white" : "bg-white"}
+            `}>
+            {value}
+        </button>
+    );
+}
